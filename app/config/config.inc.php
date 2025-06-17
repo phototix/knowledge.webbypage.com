fbeb9ef5dafc340feefe03eb4cc4a00467032d4f
phototix <phototix@hotmail.com>
2025-06-17 15:39:36 +0800

    update

---
+++ app/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("PJ_HOST")) define("PJ_HOST", "localhost");
+if (!defined("PJ_USER")) define("PJ_USER", "webbycms");
+if (!defined("PJ_PASS")) define("PJ_PASS", "#Abccy1982#");
+if (!defined("PJ_DB")) define("PJ_DB", "webbycms_knowledge");
+if (!defined("PJ_PREFIX")) define("PJ_PREFIX", "webby_");
+if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/");
+if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "/var/www/knowledge.webbypage.com/");
+if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "https://knowledge.webbypage.com/");
+if (!defined("PJ_SALT")) define("PJ_SALT", "F0RXYL3I");
+if (!defined("PJ_INSTALLATION")) define("PJ_INSTALLATION", "8C6DD2C911E7986D1A6FE622470B7C77F8EDEB438742600A27240F46E69CDC88");
+?>
