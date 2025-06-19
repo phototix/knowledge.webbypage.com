278756b42e92dfd7560d4d414b3c3ac293347e31
Brandon <brandon@kkbuddy.com>
2025-06-19 14:31:43 +0800

    Update

---
+++ api/config/database.php
@@ -0,0 +1,6 @@
+<?php
+// Database configuration
+$db_host = 'db.gateway.01.webbypage.com';
+$db_user = 'webbycms';
+$db_pass = '#Abccy1982#';
+$db_name = ''; // Database name vary by project name. This is standard accross WebbyCMS.
